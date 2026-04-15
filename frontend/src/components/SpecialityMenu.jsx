df92325abbfdd8d751facbc00a7e5cd8333e4ba7
Yashi Bansal <bansalyashi163@gmail.com>
2026-04-15 21:44:34 +0530

    banner and top doctors

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -9,7 +9,11 @@ const SpecialityMenu = () => {
         <p className='sm:w-1/3 text-center text-sm'>Connecting You to the Right Care Experts!</p>
         <div className='flex sm:justify-center gap-4 pt-5 w-full overflow-scroll'>
             {specialityData.map((item,index) => (
-                <Link to={`/doctors/${item.speciality}`} onClick={() => scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500'>
+                <Link 
+                to={`/doctors/${item.speciality}`} 
+                onClick={() => scrollTo(0,0)} 
+                key={index}
+                className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500'>
                     <img src={item.image} alt="Icon" className='w-16 mb-2 sm:w-24'/>
                     <p>{item.speciality}</p>
                 </Link>
