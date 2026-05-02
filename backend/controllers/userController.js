976fbb3b2a1e88dbd4915bb1ba12150d95118954
Yashi Bansal <bansalyashi163@gmail.com>
2026-05-02 16:19:49 +0530

    register user controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,37 @@
+import validator from 'validator';
+import bcrypt from 'bcrypt';
+import User from '../models/userModel.js';
+import jwt from 'jsonwebtoken';
+
+const registerUser = async (req,res) => {
+    try {
+        const {name, email, password} = req.body
+        if(!name || !email || !password){
+            return res.json({success:false, message:"Missing Credentials"})
+        }
+        if(password.length < 6){
+            return res.json({success:false, message:"Enter strong password"})
+        }
+        if(!validator.isEmail(email)){
+            return res.json({success:false, message:"Enter valid email"})
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        const userData = {
+            name,
+            email,
+            password:hashedPassword
+        }
+        const newUser = new User(userData)
+        const user = await newUser.save()
+        const token = jwt.sign({id:user._id},process.env.JWT_SECRET)
+        return res.json({success: true, token})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false,message: error.message})
+    }
+}
+
+
+export {registerUser}
--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,11 +1,34 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
 import { doctors } from "../assets/assets_frontend/assets";
+import axios from 'axios';
+import { toast } from "react-toastify";
 
 export const AppContext = createContext()
 
 const AppContextProvider = (props) => {
     
     const currencySymbol = '$';
+    const backendUrl = import.meta.env.VITE_BACKEND_URL;
+    const [doctors, setDoctors] = useState([])
+
+    const getDoctorsData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl+'/api/doctor/list');
+            if(data.success){
+                setDoctors(data.doctors)
+            }else{
+                toast.error(data.message)
+            }
+        } catch (error) {
+            console.log(error.message)
+            toast.error(error.message)
+        }
+    }
+
+    useEffect(() => {
+        getDoctorsData()
+    },[])
+
     const value = {
         doctors,
         currencySymbol
