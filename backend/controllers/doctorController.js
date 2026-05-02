5dbc44914442b2009d84a8307da151955e6fbe0a
Yashi Bansal <bansalyashi163@gmail.com>
2026-05-02 14:57:29 +0530

    doctors list at admin site and availability functionality

---
+++ backend/controllers/doctorController.js
@@ -0,0 +1,16 @@
+import Doctor from "../models/doctorModel.js"
+
+
+const changeAvailability = async (req,res) => {
+    try {
+        const {docId} = req.body
+        const docData = await Doctor.findById(docId)
+        await Doctor.findByIdAndUpdate(docId, {available: !docData.available})
+        return res.json({success:true, message:"Availability changed"})
+    } catch (error) {
+        console.log(error)
+        res.json({success: false,message: error.message})
+    }
+}
+
+export {changeAvailability}
